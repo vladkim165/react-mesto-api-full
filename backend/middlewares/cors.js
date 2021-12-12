a4fa06c4f985a60d19072b8786e13976de4611a7
Vladislav Kim <vladkim165@gmail.com>
2021-12-13 00:00:56 +0300

    hotfix/cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -20,9 +20,9 @@ module.exports = (req, res, next) => {
   }
 
   const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+
+  res.header('Access-Control-Allow-Origin', origin);
+
 
   next();
 };
