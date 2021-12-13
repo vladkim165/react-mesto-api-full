a58d168ccccef35b0187c862cd6652db78e9dbb4
Vladislav Kim <vladkim165@gmail.com>
2021-12-13 15:58:14 +0300

    hotfix/cookies

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -14,7 +14,6 @@ const login = (req, res, next) => {
       );
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
       });
       res.status(200).send({ message: 'Вы успешно залогинены' });
     })
